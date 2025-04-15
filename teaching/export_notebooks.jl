0cb75a6a8ace6433ec51035a6ab067f1f788cf81
Emmanuel Pilliat <epilliat@gmail.com>
2025-04-15 17:24:09 +0200

    notebook, profile update

---
+++ teaching/export_notebooks.jl
@@ -0,0 +1,14 @@
+using PlutoSliderServer
+
+cd("/home/emmanuel/epilliat.github.io/teaching")
+## Hypothesis Testing
+
+PlutoSliderServer.export_notebook("hypothesis_testing/pluto/proba_basics.jl"; Export_create_index=false)
+PlutoSliderServer.export_notebook("hypothesis_testing/pluto/distribution_comparisons.jl"; Export_create_index=false)
+PlutoSliderServer.export_notebook("hypothesis_testing/pluto/illustration_pvalue.jl"; Export_create_index=false)
+PlutoSliderServer.export_notebook("hypothesis_testing/pluto/TP_chi-squared-tests.jl"; Export_create_index=false)
+
+
+## Linear Regression
+
+PlutoSliderServer.export_notebook("linear_model/pluto/introduction.jl"; Export_create_index=false)
